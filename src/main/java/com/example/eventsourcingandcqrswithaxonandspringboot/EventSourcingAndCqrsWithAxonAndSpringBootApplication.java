9fff5817529d08f8690ada5aed4804cb7b4ca246
work <work@kali>
2025-12-01 04:45:30 +0100

    Initialize Project & Git

---
+++ src/main/java/com/example/eventsourcingandcqrswithaxonandspringboot/EventSourcingAndCqrsWithAxonAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsourcingandcqrswithaxonandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingAndCqrsWithAxonAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingAndCqrsWithAxonAndSpringBootApplication.class, args);
+    }
+
+}
